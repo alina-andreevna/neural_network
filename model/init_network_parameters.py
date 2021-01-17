e74fc4df725351d3533c260258934fa32eab16a6
Alina Andreevna <ali-piratova@mail.ru>
2021-01-17 18:20:19 +0300

    INITIALIZATION PARAMETERS FOR MODEL ADDED
    network_parameters - description of neural network
    fpga_parameters - description for FPGA implementation

---
+++ model/init_network_parameters.py
@@ -0,0 +1,21 @@
+# Input description
+INPUT_LAYER_SIZE = 5
+INPUT_ACTIVATION = ['relu']
+
+# Hidden description
+HIDDEN_LAYERS_COUNT = 3
+HIDDEN_LAYERS_SIZE = [4,3,2]
+HIDDER_LAYERS_ACTIVATIONS = ['relu', 'relu', 'relu']
+
+# Output description
+OUTPUT_LAYER_SIZE = 1
+OUTPUT_ACTIVATION = ['sigmoid']
+
+# Assertions
+assert(len(INPUT_ACTIVATION) == 1)
+
+assert(len(HIDDEN_LAYERS_SIZE) == HIDDEN_LAYERS_COUNT)
+assert(len(HIDDER_LAYERS_ACTIVATIONS) == HIDDEN_LAYERS_COUNT)
+
+assert(OUTPUT_LAYER_SIZE == 1)
+assert(len(OUTPUT_ACTIVATION) == 1)
